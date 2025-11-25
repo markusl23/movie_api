1e81ac24a7dd26ade5eae51256f16d8e1bff0c09
markusl23 <markus4github@riseup.net>
2025-11-25 16:08:40 +0100

    added movies array containing movie objects with titles & directors

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 const express = require("express");
 const app = express();
+
+let movies = [
+  {
+    title: 'Forrest Gump',
+    director: 'Robert Zemeckis'
+  },
+  {
+    title: 'Lord of the Rings',
+    director: 'Peter Jackson'
+  },
+  {
+    title: 'The Shawshank Redemption',
+    director: 'Frank Darabont'
+  },
+  {
+    title: 'Star Wars',
+    director: 'George Lucas'
+  },
+  {
+    title: 'The Matrix',
+    director: 'The Wachowskis'
+  },
+  {
+    title: 'The Martian',
+    director: 'Ridley Scott'
+  },
+  {
+    title: 'Gladiator',
+    director: 'Ridley Scott'
+  },
+  {
+    title: 'Almost Famous',
+    director: 'Cameron Crowe'
+  },
+  {
+    title: 'The Truman Show',
+    director: 'Peter Weir'
+  },
+  {
+    title: 'Point Break',
+    director: 'Kathryn Bigelow'
+  }
+];
