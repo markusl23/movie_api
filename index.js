772b79ea13d4e681edcb69b0b7bc0cbf51811ac3
markusl23 <markus4github@riseup.net>
2025-12-10 12:46:28 +0100

    fixed error

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.post('/users', [
   let errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({  errors: errors.array });
+    return res.status(422).json({  errors: errors.array() });
   }
 
   let hashedPassword = Users.hashPassword(req.body.Password);
