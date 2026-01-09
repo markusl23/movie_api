5cf6608630697a65375bc98ff95f5a664dcde1d8
markusl23 <markus4github@riseup.net>
2026-01-09 12:50:58 +0100

    added error message for empty update payload

--- index.js
+++ index.js
@@ -218,6 +218,10 @@ app.put('/users/:userid/', [
     update.Birthday = req.body.Birthday;
   }
 
+  if (Object.keys(update).length === 0) {
+    res.status(400).send('Error: ' + err + " (No input fields to update)");
+  }
+
   await Users.findOneAndUpdate({ _id: objectUserId }, {
     $set: update
   },
